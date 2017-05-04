ddf5a55c9d8d4e034472f05aa9fc41731b0171ac
fis0010 <fis0010@naver.com>
2017-05-04 10:57:50 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sj2/slimefarmer/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.sj2.slimefarmer;
+
+/**
+ * Created by SJ2 on 2017-05-04.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
